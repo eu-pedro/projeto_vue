d275e9ff079cdb996669607c471cf66275c3a38d
pedro <pedro.henrike2812@gmail.com>
2022-06-01 20:58:13 -0300

    Iniciado projeto Bootstrap com Vue

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            bloco:{
+                inicio: true,
+                idade: false,
+                cambio:false,
+                diaSemana:false,
+                desconto:false,
+            }
+            
+        }
+    
+        },
+        methods:{
+            verificarLink(classe){
+                console.log(classe)
+            }
+        }
+        
+    
+})  
+
+app.mount('#app')
